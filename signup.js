407047b02e874ba42312a73f3fabb91de810de6c
aarti0414 <apwaraikar04033@gmail.com>
2021-07-16 16:02:16 +0530

    toast notification and changes done

---
+++ signup.js
@@ -0,0 +1,43 @@
+const btn = document.getElementById("btn");
+const container = document.getElementById("container");
+
+function createNotification() {
+    const notif = document.createElement("div");
+    notif.classList.add("toast");
+
+    notif.innerText = " New User Regisered!!";
+
+    container.appendChild(notif);
+    setTimeout(() => {
+        notif.remove();
+    }, 2500);
+}
+
+function signup(e) {
+
+    const form = new FormData(e.target);
+    
+    const signupDetails = {
+        name: form.get("name"),
+        email: form.get("email"),
+        phoneno : form.get("phoneno"),
+        password: form.get("password")
+        
+    }
+    console.log(signupDetails)
+    e.preventDefault();
+    axios.post('http://localhost:3000/user/signup',signupDetails).then(response => {
+        if(response.status === 201){
+            createNotification();
+            setTimeout(()=>{
+                window.location.href = "index.html" // change the page on successful login
+
+            },3000);
+        } else {
+            throw new Error('Failed to login')
+        }
+    }).catch(err => {
+        document.body.innerHTML += `<div style="color:red;">${err} <div>`;
+    })
+}
+
